73b4a5b612425fc2235809a0b06e3e5a52ebe8cc
cyprien <cyprien.delapoeze-dharambure@etu.unicaen.fr>
2023-09-11 13:54:45 +0200

    comments

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
+//------------------------------ TIMER EXERCICE ------------------------------\\
+//------------------------------ -------------- ------------------------------\\
 
+//---------- Variable of Hours, Minutes and Secondes of the actual timer----------\\
+let Hours = document.getElementById("TravailHours").value;
+let Minutes = document.getElementById("TravailMinutes").value;
+let Secondes = document.getElementById("TravailSecondes").value;
 
-//Exercice timer------------------------------------------
-
-let heures = document.getElementById("TravailHeures").value;
-let minutes = document.getElementById("TravailMinutes").value;
-let secondes = document.getElementById("TravailSecondes").value;
+//---------- Variable of Hours, Minutes and Secondes format with the function "FormatHour" of the actual timer----------\\
 let secondesPrint;
-let minutesPrint;
-let heuresPrint;
+let MinutesPrint;
+let HoursPrint;
 
+//---------- Variable that give the actual mode between "Travail" and "Pause" ----------\\
 let travail = 1;
 
-let StopStartTimer = 0;
+//---------- Variable that give the status of the timer between "Reset" and "Start" ----------\\
+let ResetStartTimer = 0;
 
-document.getElementById("timer").innerHTML = formatHour(heures, minutes, secondes);
+//---------- Format hour and place it in the timer ----------\\
+document.getElementById("timer").innerHTML = formatHour(Hours, Minutes, Secondes);
 
-function formatHour(Heures, Minutes, Secondes, HeuresPrint, MinutesPrint, SecondesPrint) {
-    if(Heures < 10){
-        if(Heures == ""){
-            HeuresPrint = "00";
+//---------- Format hour with zero in front if needed so it take 2 number ----------\\
+function formatHour(Hours, Minutes, Secondes, HoursPrint, MinutesPrint, SecondesPrint) {
+    if(Hours < 10){
+        if(Hours == ""){
+            HoursPrint = "00";
         }
         else{
-            HeuresPrint = "0" + Heures;
+            HoursPrint = "0" + Hours;
         }
     }
     else{
-        HeuresPrint = Heures;
+        HoursPrint = Hours;
     }
     if(Minutes < 10){
         if(Minutes == ""){
@@ -49,109 +55,112 @@ function formatHour(Heures, Minutes, Secondes, HeuresPrint, MinutesPrint, Second
     else{
         SecondesPrint = Secondes;
     }
-    return HeuresPrint + " : " + MinutesPrint + " : " + SecondesPrint;
+    return HoursPrint + " : " + MinutesPrint + " : " + SecondesPrint;
 }
 
-
+//---------- Permit time to pass in the timer ----------\\
 setInterval(() => {
-    if(StopStartTimer == 1){
-        if(minutes == 0 && heures > 0){
-            minutes = 59;
-            heures -= 1;
+    if(ResetStartTimer == 1){
+        if(Minutes == 0 && Hours > 0){
+            Minutes = 59;
+            Hours -= 1;
         }
-        if(secondes == 0 && minutes > 0){
-            secondes = 59;
-            minutes -= 1;
+        if(Secondes == 0 && Minutes > 0){
+            Secondes = 59;
+            Minutes -= 1;
         }
-        else if(secondes > 0){
-            secondes -= 1;
+        else if(Secondes > 0){
+            Secondes -= 1;
         }
-        if(minutes == 0 && heures == 0 && secondes == 0 && travail == 1){
-            heures = document.getElementById("PauseHeures").value;
-            minutes = document.getElementById("PauseMinutes").value;
-            secondes = document.getElementById("PauseSecondes").value;
+        if(Minutes == 0 && Hours == 0 && Secondes == 0 && travail == 1){
+            Hours = document.getElementById("PauseHours").value;
+            Minutes = document.getElementById("PauseMinutes").value;
+            Secondes = document.getElementById("PauseSecondes").value;
             travail = 0;
             document.getElementById("Travail").classList.remove("actual");
             document.getElementById("Pause").classList.toggle("actual");
         }
-        if(minutes == 0 && heures == 0 && secondes == 0 && travail == 0){
-            heures = document.getElementById("TravailHeures").value;
-            minutes = document.getElementById("TravailMinutes").value;
-            secondes = document.getElementById("TravailSecondes").value;
+        if(Minutes == 0 && Hours == 0 && Secondes == 0 && travail == 0){
+            Hours = document.getElementById("TravailHours").value;
+            Minutes = document.getElementById("TravailMinutes").value;
+            Secondes = document.getElementById("TravailSecondes").value;
             travail = 1;
             document.getElementById("Pause").classList.remove("actual");
             document.getElementById("Travail").classList.toggle("actual");
         }
-        document.getElementById("timer").innerHTML = formatHour(heures, minutes, secondes, heuresPrint, minutesPrint, secondesPrint);
+        document.getElementById("timer").innerHTML = formatHour(Hours, Minutes, Secondes, HoursPrint, MinutesPrint, secondesPrint);
     }
 }, 10);
 
-function functStopStartTimer(){
-    if(StopStartTimer == 1){
-        StopStartTimer = 0;
+//---------- Link to Button in the timer, Start the timer or Reset and change value of timer link to the actual mod ----------\\
+function functResetStartTimer(){
+    if(ResetStartTimer == 1){
+        ResetStartTimer = 0;
         document.getElementById("stopTimer").innerText = "Start";
         if(travail == 1){
-            heures = document.getElementById("TravailHeures").value;
-            minutes = document.getElementById("TravailMinutes").value;
-            secondes = document.getElementById("TravailSecondes").value;
-            document.getElementById("timer").innerHTML = formatHour(heures, minutes, secondes, heuresPrint, minutesPrint, secondesPrint);
+            Hours = document.getElementById("TravailHours").value;
+            Minutes = document.getElementById("TravailMinutes").value;
+            Secondes = document.getElementById("TravailSecondes").value;
+            document.getElementById("timer").innerHTML = formatHour(Hours, Minutes, Secondes, HoursPrint, MinutesPrint, secondesPrint);
         }
         else{
-            heures = document.getElementById("PauseHeures").value;
-            minutes = document.getElementById("PauseMinutes").value;
-            secondes = document.getElementById("PauseSecondes").value;
-            document.getElementById("timer").innerHTML = formatHour(heures, minutes, secondes, heuresPrint, minutesPrint, secondesPrint);
+            Hours = document.getElementById("PauseHours").value;
+            Minutes = document.getElementById("PauseMinutes").value;
+            Secondes = document.getElementById("PauseSecondes").value;
+            document.getElementById("timer").innerHTML = formatHour(Hours, Minutes, Secondes, HoursPrint, MinutesPrint, secondesPrint);
         }
     }
     else{
-        StopStartTimer = 1;
+        ResetStartTimer = 1;
         document.getElementById("stopTimer").innerText = "Reset";
     }
 }
 
-document.getElementById("TravailHeures").addEventListener("change", (event) => {
+//---------- Listener on all input ----------\\
+document.getElementById("TravailHours").addEventListener("change", (event) => {
     if(travail == 1){
-        let heures = event.target.value;
-        document.getElementById("timer").innerHTML = formatHour(heures, minutes, secondes, heuresPrint, minutesPrint, secondesPrint);
+        let Hours = event.target.value;
+        document.getElementById("timer").innerHTML = formatHour(Hours, Minutes, Secondes, HoursPrint, MinutesPrint, secondesPrint);
     }
   });
 
   document.getElementById("TravailMinutes").addEventListener("change", (event) => {
     if(travail == 1){
-        minutes = event.target.value;
-        document.getElementById("timer").innerHTML = formatHour(heures, minutes, secondes, heuresPrint, minutesPrint, secondesPrint);
+        Minutes = event.target.value;
+        document.getElementById("timer").innerHTML = formatHour(Hours, Minutes, Secondes, HoursPrint, MinutesPrint, secondesPrint);
     }
   });
 
   document.getElementById("TravailSecondes").addEventListener("change", (event) => {
     if(travail == 1){
-        secondes = event.target.value;
-        document.getElementById("timer").innerHTML = formatHour(heures, minutes, secondes, heuresPrint, minutesPrint, secondesPrint);
+        Secondes = event.target.value;
+        document.getElementById("timer").innerHTML = formatHour(Hours, Minutes, Secondes, HoursPrint, MinutesPrint, secondesPrint);
     }
     });
 
-  document.getElementById("PauseHeures").addEventListener("change", (event) => {
+  document.getElementById("PauseHours").addEventListener("change", (event) => {
   	if(travail == 0){
-    	heures = event.target.value;
-    	document.getElementById("timer").innerHTML = formatHour(heures, minutes, secondes, heuresPrint, minutesPrint, secondesPrint);
+    	Hours = event.target.value;
+    	document.getElementById("timer").innerHTML = formatHour(Hours, Minutes, Secondes, HoursPrint, MinutesPrint, secondesPrint);
     }
   });
 
   document.getElementById("PauseMinutes").addEventListener("change", (event) => {
 	  if(travail == 0){
-	    	minutes = event.target.value;
-	    	document.getElementById("timer").innerHTML = formatHour(heures, minutes, secondes, heuresPrint, minutesPrint, secondesPrint);
+	    	Minutes = event.target.value;
+	    	document.getElementById("timer").innerHTML = formatHour(Hours, Minutes, Secondes, HoursPrint, MinutesPrint, secondesPrint);
 	    }
   });
 
   document.getElementById("PauseSecondes").addEventListener("change", (event) => {
     if(travail == 0){
-        secondes = event.target.value;
-        document.getElementById("timer").innerHTML = formatHour(heures, minutes, secondes, heuresPrint, minutesPrint, secondesPrint);
+        Secondes = event.target.value;
+        document.getElementById("timer").innerHTML = formatHour(Hours, Minutes, Secondes, HoursPrint, MinutesPrint, secondesPrint);
     }
   });
 
   
+//---------- Hide or make visible the inputs to modify the timer ----------\\
 let modif = 0;
 
 function functModifTimer(){
